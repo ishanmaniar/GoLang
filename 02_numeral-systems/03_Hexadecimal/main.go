91dfbd4cc8e99a1147765ab8b9a062e7e3ae0f12
IMNR <ishanmaniar@gmail.com>
2017-07-14 23:13:06 +0800

    Adding Section for Numerical System --> Decimal, binary, Hex, UTF8

---
+++ 02_numeral-systems/03_Hexadecimal/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Printf("%d - %b - %X - %x \n", 42, 42,42,42)
+	fmt.Printf("%d \t %b \t %#X \t ",42,42,42)
+}
---
+++ 02_numeral-systems/05_UTF-8/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	for i:=60;i<123;i++ {
+		fmt.Printf("%d \t %b \t %x \t %#x \t %q \n",i,i,i,i,i) //UTF-8 --> %q
+	}
+}
+
